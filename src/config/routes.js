55d4b2ab4bc70d2077f1e046dac8beb00ea0cfa9
Sai Nay Lin Thar <133788517+snlt11@users.noreply.github.com>
2026-09-21 13:16:02 +0630

    Add Today care details route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,7 @@
 export const ROUTES = Object.freeze({
   onboarding: "/",
   home: "/home",
+  careDetails: "/home/care/:taskId",
   planner: "/planner",
   health: "/health",
   profile: "/profile",
