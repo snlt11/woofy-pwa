6d1869ece5da9b8de142b6a37981afc135de9131
Sai Nay Lin Thar <133788517+snlt11@users.noreply.github.com>
2026-09-21 13:16:16 +0630

    Register Today care details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AppLayout from "./components/AppLayout";
 import InstallPrompt from "./components/InstallPrompt";
 import { ROUTES } from "./config/routes";
 import HealthPage from "./features/health/HealthPage";
+import CareDetailsPage from "./features/home/CareDetailsPage";
 import HomePage from "./features/home/HomePage";
 import OnboardingPage from "./features/onboarding/OnboardingPage";
 import PlannerPage from "./features/planner/PlannerPage";
@@ -19,6 +20,7 @@ export default function App() {
 
         <Route element={<AppLayout />}>
           <Route path={ROUTES.home} element={<HomePage />} />
+          <Route path={ROUTES.careDetails} element={<CareDetailsPage />} />
           <Route path={ROUTES.planner} element={<PlannerPage />} />
           <Route path={ROUTES.health} element={<HealthPage />} />
           <Route path={ROUTES.profile} element={<ProfilePage />} />
